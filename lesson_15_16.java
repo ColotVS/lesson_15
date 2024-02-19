d86c29d673a4fe3d55675f81076fcace089566e8
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-02-19 18:14:55 +0700

    Перехват исключения Урок 15, задача 16

---
+++ lesson_15_16.java
@@ -0,0 +1,21 @@
+public class lesson_15_16 {
+    //Перехвати исключение в методе main, чтобы программа компилировалась.
+    //При возникновении исключения нужно вывести на экран строку errorMessage.
+    public static String errorMessage = "не повезло";
+
+    public static void main(String[] args) {
+        try {
+            generateLuckyNumber();
+        } catch (Exception e) {
+            System.err.println(errorMessage);
+        }
+    }
+
+    static void generateLuckyNumber() throws Exception {
+        int luckyNumber = (int) (Math.random() * 100);
+        if (luckyNumber == 13) {
+            throw new Exception();
+        }
+        System.out.println("твое счастливое число: " + luckyNumber);
+    }
+}
