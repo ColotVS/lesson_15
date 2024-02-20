2e58ae3d5218570d7e24eef1ddb25facb08cefbd
ColotVS <kolotilov98@mail.ru>
2024-02-20 15:21:07 +0700

    Зарезервированные слова в Java Урок 15, задача 23

---
+++ lesson_15_23.java
@@ -0,0 +1,66 @@
+public class lesson_15_23 {
+    //Зарезервированные слова в Java
+
+    //В языке Java, как и в любом языке программирования, есть слова, которые имеют особый смысл. Например, return или if или while.
+    //Такие слова называются ключевыми (keywords) и считаются зарезервированными языком Java.
+
+    //Вы не можете использовать эти слова в качестве имени переменной, имени метода или класса.
+    //Компилятор всегда будет интерпретировать их строго определенным образом. Всего в Java таких слов 54.
+
+    /*
+    abstract
+    assert
+    boolean
+    break
+    byte
+    case
+    catch
+    char
+    class
+    const
+    continue
+    default
+    do
+    double
+    else
+    enum
+    extends
+    final
+    finally
+    float
+    for
+    goto
+    if
+    implements
+    import
+    instanceof
+    int
+    interface
+    long
+    native
+    new
+    package
+    private
+    protected
+    public
+    return
+    short
+    static
+    strictfp
+    super
+    switch
+    synchronized
+    this
+    throw
+    throws
+    transient
+    try
+    void
+    volatile
+    while
+    var
+    true
+    null
+    false
+    */
+}
